be341bb4e40aa1d848377462cb98bb3c60bc34a4
Yogesh Kattilath <yogeshkattilath1990@gmail.com>
2014-02-05 19:09:36 +0530

    Cart Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cart::Application.config.secret_key_base = '934352d495dc1b26f13c4428324e9fff1e4ecbf036a753cf46ec770e62440579db21141bd4373eada197b768a7fc07486f1e7e89a30fa6a75b6e8341bab4ec74'
